1e5bb3ceeb79281978b61c87a3134dbc3c90844a
Jesus Rodriguez <rodriguezjesus609@gmail.com>
2021-03-14 20:57:52 +0100

    Add app icons

---
+++ ImpactMeter/TitleAndDescriptionView.swift
@@ -0,0 +1,15 @@
+//
+//  InputView.swift
+//  ImpactMeter
+//
+//  Created by Jesus Rodriguez on 14.03.21.
+//
+
+import SwiftUI
+
+struct InputView: View {
+
+    var body: some View {
+        
+    }
+}
